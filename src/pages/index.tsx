b7c53d5474f5c74ea3d0a4af295d64a64bfcd74d
Masahiro Ogino <masahiro.o@mcdigital.jp>
2024-05-12 15:05:50 +0900

    implement basic shader

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,78 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrthographicCamera } from "@react-three/drei";
+import { Mesh, PlaneGeometry, ShaderMaterial } from "three";
+import { useMemo, useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const MainView = ({ mousePosition }: { mousePosition: number[]}) => {
+
+  const meshRef = useRef();
+
+  // ジオメトリの作成
+  const geometry = useMemo(() => new PlaneGeometry(2, 2), []);
+
+  // マテリアルの作成
+  const mat = useMemo(() => new ShaderMaterial({
+    vertexShader:
+      `
+      varying vec2 vUv;
+      void main() {
+        vUv = uv;
+        //vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+        //vec4 mvPosition =  viewMatrix * worldPosition;
+        //gl_Position = projectionMatrix * mvPosition;
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      }
+      `,
+    fragmentShader:
+      `
+      uniform float time;
+      uniform vec2 mousePosition;
+      varying vec2 vUv;
+      void main() {
+        gl_FragColor = vec4(vUv.x, vUv.y, mousePosition.x, 1.0);
+      }
+      `,
+    uniforms: {
+      time: { value: 0 },
+      mousePosition: { value: [0, 0] },
+      // その他のユニフォームを追加
+    },
+  }), []);
+
+  // アニメーションループ
+  useFrame((state, delta, xrFrame) => {
+    if (!meshRef.current) return;
+    (meshRef.current as Mesh).material.uniforms.time.value += delta;
+    //(meshRef.current as Mesh).rotation.y += 0.01;
+    (meshRef.current as Mesh).material.uniforms.mousePosition.value = mousePosition;
+  })
+
+  return (
+    <OrthographicCamera makeDefault position={[0, 0, 0]} left={-1} right={1} top={-1} bottom={1} near={0.1} far={1000} >
+      <mesh
+        ref={meshRef}
+        material={mat}
+        position={[0, 0, -1]}
+        rotation={[Math.PI, 0, 0]}
+        geometry={geometry}
+      />
+    </OrthographicCamera>
+  );
+}
+
 export default function Home() {
+  const [mousePosition, setMousePosition] = useState([0, 0]);
+  const canvasRef = useRef();
+  const onMouseMove = (e) => {
+    console.log(canvasRef.current.clientWidth, canvasRef.current.clientHeight)
+    setMousePosition([e.clientX / canvasRef.current.clientWidth, e.clientY / canvasRef.current.clientHeight]);
+  }
+
   return (
     <>
       <Head>
@@ -15,98 +82,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className={styles.gameview}>
+          <Canvas flat onMouseMove={onMouseMove} ref={canvasRef}>
+            <MainView mousePosition={mousePosition}></MainView>
+          </Canvas>
         </div>
       </main>
     </>
